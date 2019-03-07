52e8270ccd60babce34594ba33d6a0dd870f18c0
william gonzalez tellez <wagt2006@hotmail.com>
2019-03-07 12:02:37 -0600

    Nue vo contrlador de Casos

---
+++ SAC/Controllers/CasosController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAC.Controllers
+{
+    public class CasosController : Controller
+    {
+        // GET: Casos
+        public ActionResult Index()
+        {
+          
+            using (var db = new Models.dbModel())
+            { 
+
+
+            }
+            return View();
+        }
+
+        public ActionResult Caso(int Caso)
+        {
+            using (var db = new Models.dbModel())
+            {
+
+
+
+            }
+            return View();
+            //return Json(Graficos, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult TraerComentarios(int Caso)
+        {
+            using (var db = new Models.dbModel())
+            {
+
+
+
+            }
+            return View();
+            //return Json(Graficos, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult TraerComite(int Caso)
+        {
+            using (var db = new Models.dbModel())
+            {
+
+
+
+            }
+            return View();
+            //return Json(Graficos, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- SAC/Controllers/SugerenciasController.cs
+++ SAC/Controllers/SugerenciasController.cs
@@ -323,42 +323,34 @@ namespace SAC.Controllers
 
         public ActionResult ExportSugerencias()
         {
-            
+
             using (var db = new Models.dbModel()) {
                 var listaSugerencias = (from q in db.Quejas
-                                   join a in db.Area on q.CodigoDepartamento equals a.CodigoArea
-                                   select new { codigo = q.CodigoQueja, Tipo = q.CodigoTipo,Sucursal = q.CodigoSucursal, Codigo_Area = q.CodigoDepartamento,Area =  a.NombreArea,q.Empleado, q.Queja, q.Fecha }).ToList();
+                                        join a in db.Area on q.CodigoDepartamento equals a.CodigoArea
+                                        select new { codigo = q.CodigoQueja, Tipo = q.CodigoTipo, Sucursal = q.CodigoSucursal, Codigo_Area = q.CodigoDepartamento, Area = a.NombreArea, q.Empleado, q.Queja, q.Fecha }).ToList();
 
 
-         
-            var grid = new System.Web.UI.WebControls.GridView();
-            grid.DataSource = listaSugerencias;
+
+                var grid = new System.Web.UI.WebControls.GridView();
+                grid.DataSource = listaSugerencias;
                 grid.BorderStyle = System.Web.UI.WebControls.BorderStyle.None;
 
-            grid.DataBind();
+                grid.DataBind();
 
-            Response.ClearContent();
-            var fName = string.Format("Sugerencias-{0}", DateTime.Now.ToString("s"));
-            Response.AddHeader("content-disposition", "attachement; filename=" + fName  + ".xls");
-            Response.ContentType = "application/excel";
-            StringWriter sw = new StringWriter();
-            HtmlTextWriter htw = new HtmlTextWriter(sw);
-            grid.RenderControl(htw);
-            Response.Output.Write(sw.ToString());
-            Response.Flush();
-            Response.End();
+                Response.ClearContent();
+                var fName = string.Format("Sugerencias-{0}", DateTime.Now.ToString("s"));
+                Response.AddHeader("content-disposition", "attachement; filename=" + fName + ".xls");
+                Response.ContentType = "application/excel";
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new HtmlTextWriter(sw);
+                grid.RenderControl(htw);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
             }
 
             return View();
-           
-        }
 
-        public ActionResult Resolucion(int Sugerencia)
-        {
-
-
-            return View();
-            //return Json(Graficos, JsonRequestBehavior.AllowGet);
         }
     }
 }
